1151c00f3add2c7d8a479fb7911e81d7d52e38af
Saeed Ghasemi <saeed.ghasemi624@gmail.com>
2023-03-30 00:01:52 +0200

    Wrote the writer.c application and Makefile. (fully tested)

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,4 +1,15 @@
 
+#include <assert.h>
+
+#include <unistd.h>  // read()
+
+#include <stdlib.h>  // exit()
+
+#include <string.h>  // strlen()
+
+
+#include <sys/types.h>
+#include <dirent.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -20,7 +31,7 @@
 
 /**
  * @brief This read function ensures that recoverable errors are resolved before return.
- * It guarantees the return of 'len' bytes then less there is an error or EOF encounter.
+ * This function guarantees the return of 'len' bytes then-less there is an error or EOF encounter.
  * @attention Blocking function.
  *
  * @param fd            The file-descriptor to read from.
@@ -91,42 +102,89 @@ int main(int argc, char *argv[]) {
     syslog (LOG_INFO, "---- Writer app start ----");
     LOGD("---- Writer app start ----");
 
-    // Check 'writefile' input:
-    if (argc >= 1 && argv[0] != NULL) {
-        if (strcmp(argv[0], "") == 0) {
+    // Index 0 is the name of the file!
+    char* writefile = argv[1];
+    char* writestr = argv[2];  // This index may not even exist. Check "argc >= 2" before use.
 
+    syslog (LOG_DEBUG, "argc = %d", argc);
+
+    // Check 'writefile' input:
+    if (argc == 1) {
+        syslog (LOG_ERR, "Error: No input arguments were given.");
+        exit(1);
+    } else if (argc < 3) {
+        syslog (LOG_ERR, "Error: The write string argument was not given.");
+        exit(1);
+    }
+
+    syslog (LOG_DEBUG, "1");
+
+    // Check the path for being a directory:
+    DIR *dir_stream_writefile = opendir(writefile);
+    if (dir_stream_writefile != NULL) {
+        syslog (LOG_ERR, "The path given is not a file-path. (%s)", writefile);
+        if (closedir (dir_stream_writefile) != 0) {
+            syslog (LOG_ERR, "Failed to close directory stream properly.");
+        }
+        exit(1);
+    } else {
+        perror("opendir()");
+        assert(errno == ENOTDIR || errno == ENOENT);  // Just checking expectation.
+    }
+
+    size_t writestr_length = strlen(writestr);
+
+    if (argc < 2 || writestr == NULL || writestr_length == 0) {
+        syslog (LOG_ERR, "Error: The write string argument was not given.");
+    }
+    // Stating operation:
+    syslog (LOG_DEBUG, "Writing %s to %s.", writestr, writefile);
+
+    /*
+     * Write to file:
+     */
+
+    // open with read, write, exec for owner and read only for group and other/everyone else.
+    int fd = open(writefile, O_RDWR | O_APPEND | O_CREAT | O_TRUNC, 0644);  // Note: The umask is 0002.
+    syslog (LOG_DEBUG, "fd = %d / errno = %d", fd, errno);
+
+    if (fd == -1) {
+        syslog (LOG_ERR, "Error: open() failed");
+    }
+
+
+    ssize_t byte_count_written = write (fd, writestr, writestr_length);
+
+    if (byte_count_written == -1) {
+        /* error, check errno */
+        perror("write() failed");
+        syslog (LOG_ERR, "Could not write to file.");
+        exit(1);
+    } else if (((size_t)byte_count_written) != writestr_length) {
+        /* possible error, but 'errno' not set */
+        if(byte_count_written != 0) {
+            syslog (LOG_ERR, "Error: Partial write performed.");
+            exit(1);
         }
     }
 
-    int fd = open("/home/kidd/madagascar", O_RDWR | O_APPEND | O_CREAT | O_EXCL | O_TRUNC, 0644);  // Note: The umask is 0002.
-    if (fd == -1) {
-        syslog (LOG_ERR, "open() failed");
-    } else {
-        assert(errno == 0);  // just in case
+    if (close (fd) == -1) {
+        perror("Error: close() failed");
     }
 
-    unsigned long word;  // Read buffer.
-    int len = sizeof(unsigned long);  // Number of bytes to read.
-    ssize_t bytesRead = read_complete_segment(fd, &word, len);
-    if (bytesRead == -1) {
-        perror ("read() failed");  // Print the error description.
-        syslog(LOG_ERR, "read() failed");  // Not recoverable error.
-    } else if (bytesRead == -2) {
-        perror ("read() failed (non-blocking read was issued)");  // Print the error description.
-        syslog(LOG_ERR, "read() failed (non-blocking read was issued)");  // Not recoverable error.
-    }
+    // unsigned long buf_word;  // Read buffer.
+    // int buf_len = sizeof(unsigned long);  // Number of bytes to read.
+    // ssize_t bytesRead = read_complete_segment(fd, &buf_word, buf_len);
+    // if (bytesRead == -1) {
+    //     perror ("Error: read() failed");  // Print the error description.
+    //     syslog(LOG_ERR, "Error: read() failed");  // Not recoverable error.
+    // } else if (bytesRead == -2) {
+    //     perror ("Error: read() failed (non-blocking read was issued)");  // Print the error description.
+    //     syslog(LOG_ERR, "Error: read() failed (non-blocking read was issued)");  // Not recoverable error.
+    // }
 
 
 
 
-// if [[ $writefile == "" ]]
-// then
-//     echo "Error: No input arguments were given."
-//     exit 1
-// elif [[ $writestr == "" ]]
-// then
-//     echo "Error: The write string argument was not given."
-//     exit 1
-// fi
 }
 
